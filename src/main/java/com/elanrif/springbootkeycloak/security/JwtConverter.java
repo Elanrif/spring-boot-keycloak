89ae4080e675bef76b0de6f38abaa17082e024cc
Elanrif <saidbacoelanrif@gmail.com>
2026-01-16 15:00:42 +0100

    fix: jwt updated methods

--- src/main/java/com/elanrif/springbootkeycloak/security/JwtConverter.java
+++ src/main/java/com/elanrif/springbootkeycloak/security/JwtConverter.java
@@ -1,6 +1,7 @@
 package com.elanrif.springbootkeycloak.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.elanrif.springbootkeycloak.security.JwtConverterProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
@@ -18,11 +19,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
+@RequiredArgsConstructor
 public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-    @Autowired
-    private JwtConverterProperties jwtConverterProperties;
+    private final JwtConverterProperties jwtConverterProperties;
 
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
@@ -30,30 +31,31 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
                 jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
                 extractResourceRoles(jwt).stream()
         ).collect(Collectors.toSet());
+
         return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
     }
 
     private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        Map<String, Object> resourceAccess = jwt.getClaimAsMap("resource_access");
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
         Map<String, Object> resource;
         Collection<String> resourceRoles;
 
-        if(resourceAccess == null
+        if (resourceAccess == null
                 || (resource = (Map<String, Object>) resourceAccess.get(jwtConverterProperties.getResourceId())) == null
-                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null
+        ) {
             return Set.of();
         }
 
         return resourceRoles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                 .collect(Collectors.toSet());
-
     }
 
     private String getPrincipalClaimName(Jwt jwt) {
         String claimName = JwtClaimNames.SUB;
 
-        if(jwtConverterProperties.getPrincipalAttribute() != null) {
+        if (jwtConverterProperties.getPrincipalAttribute() != null) {
             claimName = jwtConverterProperties.getPrincipalAttribute();
         }
         return jwt.getClaim(claimName);
--- src/main/java/com/elanrif/springbootkeycloak/security/SecurityConfig.java
+++ src/main/java/com/elanrif/springbootkeycloak/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.elanrif.springbootkeycloak.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,25 +16,18 @@ public class SecurityConfig {
 
     public static final String ADMIN = "admin";
     public static final String USER = "user";
-    @Autowired
-    private JwtConverter jwtConverter;
+    private final JwtConverter jwtConverter;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(HttpMethod.GET,"/api/admin/**").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.GET,"/api/user/**").hasAnyRole(ADMIN, USER)
-                .requestMatchers(HttpMethod.GET,"/api/home").permitAll()
-                    .requestMatchers("/error").permitAll() // <-- autoriser la page d'erreur
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt
-                    .jwtAuthenticationConverter(jwtConverter)
-                )
-            );
-        return http.build();
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/home").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/user/**").hasRole(USER)
+                        .requestMatchers(HttpMethod.GET, "/api/admin/**").hasRole(ADMIN)
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
+                .build();
     }
 }

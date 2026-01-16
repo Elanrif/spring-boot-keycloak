b3380fb76c9819ca8f800d251684fba2ace1b472
Elanrif <saidbacoelanrif@gmail.com>
2026-01-16 15:46:47 +0100

    fix: securityConfig with return build

--- src/main/java/com/elanrif/springbootkeycloak/security/SecurityConfig.java
+++ src/main/java/com/elanrif/springbootkeycloak/security/SecurityConfig.java
@@ -20,14 +20,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
+        http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/home").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/user/**").hasRole(USER)
                         .requestMatchers(HttpMethod.GET, "/api/admin/**").hasRole(ADMIN)
+                        .requestMatchers(HttpMethod.GET, "/api/user/**").hasAnyRole(ADMIN, USER)
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
-                .build();
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+
+        return http.build();
     }
 }

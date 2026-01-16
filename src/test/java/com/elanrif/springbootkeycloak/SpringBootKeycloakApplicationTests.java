0fb09caae37c0046c29fc58026454cf227df166f
Elanrif <saidbacoelanrif@gmail.com>
2026-01-16 12:42:51 +0100

    feat: spring boot security with keycloak

---
+++ src/test/java/com/elanrif/springbootkeycloak/SpringBootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elanrif.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
